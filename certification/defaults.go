c9e25d636a790fa97975a4bdab8e26205331887c
Brandon Palm <bpalm@redhat.com>
2022-09-27 14:38:57 -0500

    More copying of files

---
+++ certification/defaults.go
@@ -0,0 +1,10 @@
+package certification
+
+var (
+	DefaultCertImageFilename   = "cert-image.json"
+	DefaultRPMManifestFilename = "rpm-manifest.json"
+	DefaultTestResultsFilename = "results.json"
+	DefaultPyxisHost           = "catalog.redhat.com/api/containers"
+	DefaultPyxisEnv            = "prod"
+	SystemdDir                 = "/etc/systemd/system"
+)
---
+++ certification/types.go
@@ -0,0 +1,13 @@
+package certification
+
+import v1 "github.com/google/go-containerregistry/pkg/v1"
+
+// ImageReference holds all things image-related
+type ImageReference struct {
+	ImageURI        string
+	ImageFSPath     string
+	ImageInfo       v1.Image
+	ImageRepository string
+	ImageRegistry   string
+	ImageTagOrSha   string
+}
